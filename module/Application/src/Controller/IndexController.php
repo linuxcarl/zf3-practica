50caddde7dcac3e985e04cec553617e9b0d75de6
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-05-13 14:28:23 -0500

    se inico el proyecto en zf3 con una pequena prueba en la vista lista

---
+++ module/Application/src/Controller/IndexController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+    public function listaAction()
+    {
+        return [
+            "empleados" => [
+                "nombre" => "Carlos",
+                'apellido' => "Ramirez"
+            ],
+            "titulo" => "Lista de empleados"
+        ];
+    }
+}
